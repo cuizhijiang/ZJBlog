0079aff0cd8fc69c35c5f22c51a9cb94afd3209f
czhj <zj_cui@foxmail.com>
2016-02-17 15:05:10 +0800

    markdown格式转成html方法

---
+++ src/Extensions/HtmlExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MarkdownSharp;
+
+namespace System.Web.Mvc
+{
+    public static class HtmlExtensions
+    {
+        //markdown格式转成html
+        public static MvcHtmlString TransformToHtml(this HtmlHelper html,string markdown)
+        {
+            Markdown mark = new Markdown();
+            var str = mark.Transform(markdown);
+            return MvcHtmlString.Create(str);
+        } 
+    }
+}
