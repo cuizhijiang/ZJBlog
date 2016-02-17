83c35a87234ba787780507a2fb2755d5894864eb
czhj <zj_cui@foxmail.com>
2016-02-17 16:25:01 +0800

    Renamed TransformToHtml to ToHtml

--- src/Extensions/HtmlExtensions.cs
+++ src/Extensions/HtmlExtensions.cs
@@ -5,7 +5,7 @@ namespace System.Web.Mvc
     public static class HtmlExtensions
     {
         //markdown格式转成html
-        public static MvcHtmlString TransformToHtml(this HtmlHelper html,string markdown)
+        public static MvcHtmlString ToHtml(this HtmlHelper html,string markdown) 
         {
             Markdown mark = new Markdown();
             var str = mark.Transform(markdown);
